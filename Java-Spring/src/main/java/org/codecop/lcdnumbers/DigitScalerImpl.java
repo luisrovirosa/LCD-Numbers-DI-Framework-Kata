5eb899cb817970785646fc4b52d2d99a099a91b7
Luis Rovirosa Mairlot <luis@codium.team>
2020-07-01 19:45:56 +0200

    DigitScalerImpl is not a service anymore

--- Java-Spring/src/main/java/org/codecop/lcdnumbers/DigitScalerImpl.java
+++ Java-Spring/src/main/java/org/codecop/lcdnumbers/DigitScalerImpl.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service;
 /**
  * Scales x and y of digit lines.
  */
-@Service
 public class DigitScalerImpl implements DigitScaler {
 
     private ScalingRepeater repeater;
