d29ea6b9d7dce4d7e5f19e9e706aa7c97a0b0304
Luis Rovirosa Mairlot <luis@codium.team>
2020-07-01 20:27:53 +0200

    Main extract method to isolate the usage of Spring run method

--- Java-Spring/src/main/java/org/codecop/lcdnumbers/Main.java
+++ Java-Spring/src/main/java/org/codecop/lcdnumbers/Main.java
@@ -17,7 +17,10 @@ public class Main implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) {
 
-        List<String> nonOptionArgs = args.getNonOptionArgs();
+        doRun(args.getNonOptionArgs());
+    }
+
+    private void doRun(List<String> nonOptionArgs) {
         if (nonOptionArgs.size() == 0) {
             System.out.println("Run this class to see LCD Numbers working:");
             System.out.println("\nRunning the generated jar:");
