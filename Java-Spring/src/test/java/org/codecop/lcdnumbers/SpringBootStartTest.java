17a32035076856fac4a0b8211216d81d2a1fea62
Luis Rovirosa Mairlot <luis@codium.team>
2020-07-01 20:41:33 +0200

    SpringBootStartTest is not coupled with spring

--- Java-Spring/src/test/java/org/codecop/lcdnumbers/SpringBootStartTest.java
+++ Java-Spring/src/test/java/org/codecop/lcdnumbers/SpringBootStartTest.java
@@ -1,23 +1,22 @@
 package org.codecop.lcdnumbers;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.system.CapturedOutput;
-import org.springframework.boot.test.system.OutputCaptureExtension;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.Arrays;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.endsWith;
 
-@ExtendWith(OutputCaptureExtension.class)
 public class SpringBootStartTest {
 
     @Test
-    public void lcdAllDigitsOfSize2(CapturedOutput output) throws IOException {
-
+    public void lcdAllDigitsOfSize2() throws IOException {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
         new Main().doRun(Arrays.asList("1234567890", "2"));
-        assertThat(output.getOut(), endsWith(Input.join(Input.allDigitsSize2())));
+        assertThat(outputStream.toString(), endsWith(Input.join(Input.allDigitsSize2())));
     }
 }
