19730c423544417204dff1897676ab81446d5e0a
Luis Rovirosa Mairlot <luis@codium.team>
2020-07-01 20:32:48 +0200

    SpringBootStartTest reduces the dependencies on spring

--- Java-Spring/src/test/java/org/codecop/lcdnumbers/SpringBootStartTest.java
+++ Java-Spring/src/test/java/org/codecop/lcdnumbers/SpringBootStartTest.java
@@ -2,12 +2,11 @@ package org.codecop.lcdnumbers;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.DefaultApplicationArguments;
 import org.springframework.boot.test.system.CapturedOutput;
 import org.springframework.boot.test.system.OutputCaptureExtension;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.endsWith;
@@ -18,7 +17,7 @@ public class SpringBootStartTest {
     @Test
     public void lcdAllDigitsOfSize2(CapturedOutput output) throws IOException {
 
-        new Main().doRun(((ApplicationArguments) new DefaultApplicationArguments("1234567890", "2")).getNonOptionArgs());
+        new Main().doRun(Arrays.asList("1234567890", "2"));
         assertThat(output.getOut(), endsWith(Input.join(Input.allDigitsSize2())));
     }
 }
