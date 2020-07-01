b1a90606f44d53b0c4490c6d27847452917012f8
Luis Rovirosa Mairlot <luis@codium.team>
2020-07-01 20:34:08 +0200

    LcdMainTest remove the dependencies with Spring

--- Java-Spring/src/test/java/org/codecop/lcdnumbers/LcdMainTest.java
+++ Java-Spring/src/test/java/org/codecop/lcdnumbers/LcdMainTest.java
@@ -2,7 +2,6 @@ package org.codecop.lcdnumbers;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.test.system.CapturedOutput;
 import org.springframework.boot.test.system.OutputCaptureExtension;
 
@@ -10,26 +9,20 @@ import java.io.IOException;
 import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class LcdMainTest {
 
-    private ApplicationArguments args;
 
     private Main lcdMain;
 
     public LcdMainTest() {
-        args = mock(ApplicationArguments.class);
         lcdMain = new Main();
     }
 
     @Test
     @ExtendWith(OutputCaptureExtension.class)
     public void lcdAllDigitsOfSize2(CapturedOutput output) throws IOException {
-        when(args.getNonOptionArgs()).thenReturn(Arrays.asList("1234567890", "2"));
-
-        lcdMain.doRun(args.getNonOptionArgs());
+        lcdMain.doRun(Arrays.asList("1234567890", "2"));
 
         assertEquals(Input.join(Input.allDigitsSize2()), output.getOut());
     }
@@ -37,9 +30,8 @@ public class LcdMainTest {
     @Test
     @ExtendWith(OutputCaptureExtension.class)
     public void lcdAllDigitsDefaultSize(CapturedOutput output) throws IOException {
-        when(args.getNonOptionArgs()).thenReturn(Arrays.asList("1234567890"));
 
-        lcdMain.doRun(args.getNonOptionArgs());
+        lcdMain.doRun(Arrays.asList("1234567890"));
 
         assertEquals(Input.join(Input.allDigitsSize1()), output.getOut());
     }
