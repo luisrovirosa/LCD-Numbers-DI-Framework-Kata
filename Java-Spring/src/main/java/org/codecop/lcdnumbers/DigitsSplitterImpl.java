7f6c1aee0cd5d52232f2e5abeac00a0570a78915
Luis Rovirosa Mairlot <luis@codium.team>
2020-07-01 19:55:16 +0200

    DigitsSplitterImpl is not a service anymore

--- Java-Spring/src/main/java/org/codecop/lcdnumbers/DigitsSplitterImpl.java
+++ Java-Spring/src/main/java/org/codecop/lcdnumbers/DigitsSplitterImpl.java
@@ -1,14 +1,11 @@
 package org.codecop.lcdnumbers;
 
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.stream.Collectors;
 
 /**
  * Splits into digits and converts digits to LCD digits.
  */
-@Service
 public class DigitsSplitterImpl implements DigitsSplitter {
 
     private NumeralSystem numeralSystem;
