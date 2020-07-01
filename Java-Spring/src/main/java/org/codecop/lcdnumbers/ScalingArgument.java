bdcd7d5efc154182c17b63adb9fd4dde2c3517dc
Luis Rovirosa Mairlot <luis@codium.team>
2020-07-01 20:26:18 +0200

    ScalingArgument change field

--- Java-Spring/src/main/java/org/codecop/lcdnumbers/ScalingArgument.java
+++ Java-Spring/src/main/java/org/codecop/lcdnumbers/ScalingArgument.java
@@ -6,14 +6,13 @@ import java.util.List;
 
 public class ScalingArgument {
 
-    private ApplicationArguments args;
+    private List<String> nonOptionArgs;
 
     public ScalingArgument(ApplicationArguments args) {
-        this.args = args;
+        nonOptionArgs = args.getNonOptionArgs();
     }
 
     public Scaling getScaling() {
-        List<String> nonOptionArgs = args.getNonOptionArgs();
         Scaling scaling = nonOptionArgs.size() > 1 ? Scaling.of(Integer.parseInt(nonOptionArgs.get(1))) : Scaling.NONE;
         return scaling;
     }
